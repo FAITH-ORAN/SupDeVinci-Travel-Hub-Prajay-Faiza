eebafd12875a6e6c135ffa40a723a80613458a25
Prajay-Chaudhary <prajaychaudhary5678@gmail.com>
2025-04-29 15:21:27 +0200

    fix: refactor code

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,25 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require('dotenv').config()
+const app = require('./app')
+const connectMongo = require('./src/config/mongo.config')
+const redisClient = require('./src/config/redis.config')
 const authRoute = require('./src/routes/authRoute');
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
+const PORT = process.env.PORT || 3000
 app.use('/', authRoute);
 
-const PORT = process.env.PORT || 3000;
-app.use('/', authRoute);
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
-});
+async function start() {
+  try {
+    await connectMongo()
+    await redisClient.connect()
+    console.log('✅ Connected to Redis Cloud')
+
+    app.listen(PORT, () => {
+      console.log(`✅ Server running on http://localhost:${PORT}`)
+    })
+  } catch (err) {
+    console.error('❌ Server failed to start:', err.message)
+    process.exit(1)
+  }
+}
+
+start()
--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,5 @@
 const { v4: uuidv4 } = require('uuid');
-const redisClient = require('../config/redis');
+const redisClient = require('../config/redis.config');
 
 exports.login = async (req, res) => {
   try {

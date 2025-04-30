dabb2a46cc40649cbd26ed88e1e751d4c9d39141
Faiza <berrichif0717@gmail.com>
2025-04-30 10:55:26 +0200

    add index on hotel name

--- backend/src/services/mongo.service.js
+++ backend/src/services/mongo.service.js
@@ -1,7 +1,13 @@
 const Offer = require('../models/offer.model')
 
-exports.findOffers = async (from, to, limit) => {
-  return Offer.find({ from, to }).sort({ price: 1 }).limit(limit).lean()
+exports.findOffers = async (from, to, limit, q) => {
+  const query = { from, to }
+
+  if (q) {
+    query.$text = { $search: q } // recherche textuelle sur hotel.name
+  }
+
+  return Offer.find(query).sort({ price: 1 }).limit(limit).lean()
 }
 
 exports.findOfferById = async (id) => {
